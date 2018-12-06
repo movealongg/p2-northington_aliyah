03caedc34a3ec8fdf4242cab389804ce1213273f
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2018-12-05 19:02:59 -0600

    Create variables for the hidden number sections

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ $(document).ready(function() {
 	});
 });
 
-const colors = document.querySelectorAll('.colorContainer > p');
 const colorBg = document.querySelector('.color');
+const colorBg2 = document.querySelector('.color2');
+const colorBg3 = document.querySelector('.color3');
+const colors = document.querySelectorAll('.colorContainer > p');
+const colors2 = document.querySelectorAll('.colorContainer > p');
+const colors3 = document.querySelectorAll('.colorContainer > p');
 
 function wrapper(){
 	const changeColor = () => {
